c576b1f8b87c3dcf3b1e2d2f32330ebc263d73e9
nshpam <97942535+nshpam@users.noreply.github.com>
2022-12-18 19:04:08 +0700

    Create EX4.py

---
+++ week1/EX4.py
@@ -0,0 +1,21 @@
+mylist = [1,1,2]
+templist = []
+count = 0
+check = False
+
+for i in mylist:
+    templist = mylist[count+1:]
+    count+=1
+    
+    #checking list is empty
+    if templist == []:
+        break
+    
+    #found the duplicate number
+    for j in templist:
+        if i == j:
+            check = True
+            print ('Yes')
+
+if check != True:
+    print('No')
