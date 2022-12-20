9e8bbc4df389955c281cd407d2b0527e61470d87
nshpam <pamza2545@gmail.com>
2022-12-20 19:39:24 +0700

    fix ex5 algorithm

--- week1/EX5.py
+++ week1/EX5.py
@@ -1,21 +1,33 @@
 mylist = [0,-1,2,-3,1,-2]
+# mylist.sort()
+# print(mylist)
 
 def FindZeroSum(mylist):
-    num = 0
-    result = ''
-    count_first = 0
-    count_last = -1
+    nums_list = []
+    sum_list = []
+    nums_list_2 = []
 
     for i in range(len(mylist)):
+        for j in range(i+1, len(mylist)):
+            nums_list.append([mylist[i], mylist[j]])
+            sum_list.append(mylist[i] + mylist[j])
 
-        if count_first == int(len(mylist)/2):
+    for i in range(len(mylist)):
+        for j in range(len(sum_list)):
+            if mylist[i] == -sum_list[j] and mylist[i] != nums_list[j][0] and mylist[i] != nums_list[j][1]:
+                nums_list_2.append(sorted([nums_list[j][0], nums_list[j][1],mylist[i]]))
+                break
+
+    nums_list_2.sort()
+    nums_list_2.append([])
+
+    result = ''
+
+    for i in range(len(nums_list_2)):
+        if i == len(nums_list_2)-1:
             print(result[:-1])
             break
-        num  = mylist[count_first] + mylist[count_last]
-        
-        for j in range(len(mylist)):
-            if mylist[j] == -num and mylist[j] != mylist[count_first] and mylist[j] != mylist[count_last]:
-                result += "(%d %d %d),"%(mylist[count_last],mylist[count_first],mylist[j])
+        if nums_list_2[i] != nums_list_2[i+1]:
+            result += '(%d, %d, %d),'%(nums_list_2[i][0], nums_list_2[i][1], nums_list_2[i][2])
 
-        count_first+=1
-        count_last-=1
+FindZeroSum(mylist)
--- week1/unittest_EX5.py
+++ week1/unittest_EX5.py
@@ -2,3 +2,5 @@ import EX5
 import unittest
 
 class FindZeroSumUnitTest(unittest.TestCase):
+    
+    pass
