71890e4759a7be2d73d7478941109dda150a79de
nshpam <pamza2545@gmail.com>
2022-12-21 11:43:20 +0700

    update : comment unittest_EX5.py

--- week1/unittest_EX5.py
+++ week1/unittest_EX5.py
@@ -3,32 +3,46 @@ import unittest
 
 class FindZeroSumUnitTest(unittest.TestCase):
 
-    #test sorted and not sorted
+    #not sorted
     def test_not_sorted(self):
         check = EX5.FindZeroSum([0,-1,2,-3,1,-2])
         assert check == '(-3, 1, 2),(-2, 0, 2),(-1, 0, 1)'
+
+    #sorted
     def test_sorted(self):
         check = EX5.FindZeroSum([-3,-2,-1,0,1,2])
         assert check == '(-3, 1, 2),(-2, 0, 2),(-1, 0, 1)'
 
     #test repeat
+
+    #not sorted
     def test_not_sorted_repeated(self):
         check = EX5.FindZeroSum([0,-1,2,-3,1,-2,-1])
         assert check == '(-3, 1, 2),(-2, 0, 2),(-1, 0, 1)'
+    
+    #sorted
     def test_sorted_repeated(self):
         check = EX5.FindZeroSum([-3,-2,-1,0,0,1,2])
         assert check == '(-3, 1, 2),(-2, 0, 2),(-1, 0, 1)'
+    
+    #all repeat
     def test_all_repeated(self):
         check = EX5.FindZeroSum([-3,-3,-3,-3,-3])
         assert check == 'There are no number that can be sum into 0.'
     
     #test data type
+
+    #empty list
     def test_empty_list(self):
         check = EX5.FindZeroSum([])
         assert check == 'There are no number that can be sum into 0.'
+
+    #none
     def test_none(self):
         check = EX5.FindZeroSum(None)
         assert check == 'Invalid data type'
+
+    #invalid type
     def test_invalid_type(self):
         check = EX5.FindZeroSum('')
         assert check == 'Invalid data type'
