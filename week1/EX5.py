8eee5414972b38f67c62b19fe2037dca0039cf44
nshpam <pamza2545@gmail.com>
2022-12-21 11:59:13 +0700

    update : more comment for EX5.py

--- week1/EX5.py
+++ week1/EX5.py
@@ -11,13 +11,25 @@ def FindZeroSum(mylist):
     nums_list_2 = []
     result = ''
 
+    #iterate first number
     for i in range(len(mylist)):
+
+        #iterate second number
         for j in range(i+1, len(mylist)):
+
+            #list of first two numbers
             nums_list.append([mylist[i], mylist[j]])
+
+            #list of two sum
             sum_list.append(mylist[i] + mylist[j])
 
+    #iterate thrid number
     for i in range(len(mylist)):
+
+        #iterate first two sum
         for j in range(len(sum_list)):
+
+            #if the third number is inverse of two sum
             if mylist[i] == -sum_list[j] and mylist[i] != nums_list[j][0] and mylist[i] != nums_list[j][1]:
                 nums_list_2.append(sorted([nums_list[j][0], nums_list[j][1],mylist[i]]))
                 break
@@ -25,6 +37,9 @@ def FindZeroSum(mylist):
     nums_list_2.sort()
     nums_list_2.append([])
 
+    #remove repeat cases
+
+    #iterate the result
     for i in range(len(nums_list_2)):
         if i == len(nums_list_2)-1:
             result = result[:-1]
