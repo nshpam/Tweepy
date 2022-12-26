dcb2c75a755a69d1d9758f7a306b663a7f648009
nshpam <pamza2545@gmail.com>
2022-12-26 08:11:13 +0700

    Update config.py

--- week2/config.py
+++ week2/config.py
@@ -1,18 +1,17 @@
 #twitter developer token
 consumer_key = 'Zsj2fTEUS4IU9h7wJhA2CeaKt' #API Key
 consumer_secret = 'GeukQOhgWKKHHKHLTh0wGjcwixl3p6KxYqxIc8uqrNoRMFVoaW' #API Key Secret
-access_token = '3302501826-yxgMxO6n1A1aXmVXS45x7zHP8FLkcSitvNyEQw4'
-access_token_secret = 'CwVEYsPoXpHJvYzbZvxlKv3mC8XlyYynmDRgsKLxauTMu'
+access_token = '3302501826-yxgMxO6n1A1aXmVXS45x7zHP8FLkcSitvNyEQw4' #access token
+access_token_secret = 'CwVEYsPoXpHJvYzbZvxlKv3mC8XlyYynmDRgsKLxauTMu' #access token secret
 
 #database settings
-mongo_client = 'mongodb://localhost:27017/'
-database_name = "twitter_data"
-collection_name = "tweets"
+mongo_client = 'mongodb://localhost:27017/' #mongodb server
+database_name = "twitter_data" #database name
+collection_name = "tweets" #collection name
 
 #search settings
-search_word = '#รีวิว'
-search_lang = "th"
-search_mode = "extended"
-search_type = "recent" #recent | popular | mixed
-num_tweet = 10
-local_timezone = "Thailand/Bangkok"
+search_word = '#รีวิว' #search word
+search_mode = "extended" #search mode
+search_type = "recent" #serach type ( recent | popular | mixed )
+num_tweet = 10 #search limit
+local_timezone = "Thailand/Bangkok" #local timezones
