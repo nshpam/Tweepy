d3f19ae46a85d8090c8cdb8b90a11cedeb506a72
nshpam <pamza2545@gmail.com>
2022-12-26 08:08:56 +0700

    update : comment tweepy_main.py

--- week2/tweepy_main.py
+++ week2/tweepy_main.py
@@ -3,22 +3,30 @@ import pymongo
 from dateutil import tz
 import config
 
-#connect to database
+#connect to mongodb with pymongo
 myclient = pymongo.MongoClient(config.mongo_client)
+#database name
 mydb = myclient[config.database_name]
+#collection name
 mycol = mydb[config.collection_name]
 
+#convert timezone from UTC to GMT
+#fixed time zone
 def convert_timezone(from_zone, to_zone, convert_date):
-   #fixed time zone
+   #convert timezone and change format into day-month-year | hour-minute
    convert_date = convert_date.replace(tzinfo=from_zone).astimezone(to_zone).strftime('%d-%m-%Y | %H:%M')
    return convert_date
 
+#insert object into database
 def insert_database(data, collection_variable):
+   #command for insert object to mongodb
+   #insert to the specified collection
    cmd = collection_variable.insert_one(data)
    return cmd
 
-#connect to twitter
+#connect to twitter API
 def connect_twitter():
+   #input the tokens
    auth = tweepy.OAuth1UserHandler(
       config.consumer_key, config.consumer_secret, config.access_token, config.access_token_secret
    )
@@ -28,25 +36,26 @@ def connect_twitter():
 def search_twiter(api):
 
    #use Cursor to serach
+   #tweepy.Cursor(search API, word + filter, search mode, search type).items(search limit)
    tweets = tweepy.Cursor(
       api.search_tweets ,
       q=config.search_word + ' -filter:retweets', 
-      # lang=config.search_lang,
-      tweet_mode=config.search_mode
-      # result_type=config.search_type
+      tweet_mode=config.search_mode,
+      result_type=config.search_type
       ).items(config.num_tweet)
 
+   #create a list of Tweets
    tweets_list = [tweet for tweet in tweets]
    count_tweets = 0
+   #timezone of your variable
    from_zone = tz.gettz('UTC')
+   #timezone you want to convert
    to_zone = tz.gettz(config.local_timezone)
 
+   #iterate the Tweet in tweets_list
    for tweet in tweets_list:
       #tweet author
       tweet_username = tweet.user.screen_name
-
-      #the unique identifier of the requested Tweet
-      tweet_id = tweet.id
 
       #tweet date
       tweet_date = tweet.created_at
@@ -54,12 +63,13 @@ def search_twiter(api):
       #convert time zone from UTC to GMT+7
       tweet_date = convert_timezone(from_zone, to_zone, tweet_date)
 
-      #tweet text
+      #get tweet text
       try:
          tweet_text = tweet.retweeted_status.full_text
       except AttributeError:
          tweet_text = tweet.full_text
       
+      #create tweet object
       tweet_object = {
          'username' : tweet_username, 
          'date' : tweet_date, 
@@ -67,7 +77,9 @@ def search_twiter(api):
       print(tweet_object)
 
       #insert to database
-      # insert_database(tweet_object, mycol)
+      insert_database(tweet_object, mycol)
+
+      #count Tweet that is inserted
       count_tweets+=1
 
    finish_text = 'total twitter : %d'%count_tweets
