1cf7485e46bdea5830c41c98e3f79467323d6f51
tw94sh <dsph2011@gmail.com>
2023-02-12 23:42:17 +0700

    top 10 word chart

--- week2/GUI_main.py
+++ week2/GUI_main.py
@@ -7,9 +7,13 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
+
 import pymongo
 import altair as alt
 import pandas as pd
+
+import plotly.express as px
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -30,38 +34,50 @@ class MainWindow(QMainWindow):
 
         # retrieve data from a MongoDB collection and store it in a Pandas dataframe
         data = list(collection.find({}))
-        df = pd.DataFrame(data)
+        source = pd.DataFrame({"word":list(data[0].keys())[1:11], "count":list(data[0].values())[1:11]})
+        source = source.sort_values(by='count', ascending=True)
+        print(source)
+
+        # Plot bar chart using Plotly
+        fig = px.bar(source, x='count', y='word', title='Ranking of words in tweets', text='count', orientation='h')
+        fig.update_traces(textposition='auto', marker=dict(color='blue'))
+        fig.update_layout(
+            title=dict(font=dict(size=24)),
+            xaxis=dict(title=dict(font=dict(size=18)), tickfont=dict(size=14)),
+            yaxis=dict(title=dict(font=dict(size=18)), tickfont=dict(size=14)),
+        )
+
+        # Save the chart to an HTML file
+        fig.write_html('topwords_bar_chart.html')
         
-        # Extract the word and count columns
-        words = df.get("word")
-        counts = df.get("count")
+        # # check the location of the current working directory
+        # print(os.getcwd())
         
-        # Plot bar chart using Altair
-        chart = alt.Chart({"word": words, "count": counts}).mark_bar().encode(
-            x='word',
-            y='count'
-        ).properties(title='Ranking of words in tweets')
-        chart.save("bar_chart.png")
+        # view = QtWebEngineWidgets.QWebEngineView()
+        # view.load(QtCore.QUrl().fromLocalFile(os.path.split(os.path.abspath(__file__))[0]))
         
-        # Show the chart legend
-        chart.legend().setVisible(True)
-        chart.legend().setAlignment(Qt.AlignBottom)
+        # view.show()
         
-        # Set chart renderer and theme
-        self.ui.chart_view = QtCharts.QChartView(chart)
-        self.ui.chart_view.setRenderHint(QPainter.Antialiasing)
-        self.ui.chart_view.chart().setTheme(QtCharts.QChart.ChartThemeDark)
+        # # Show the chart legend
+        # chart.legend().setVisible(True)
+        # chart.legend().setAlignment(Qt.AlignBottom)
         
-        # Set chart size policy
-        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.ui.chart_view.sizePolicy().hasHeightForWidth())
-        self.ui.chart_view.setSizePolicy(sizePolicy)
+        # # Set chart renderer and theme
+        # self.ui.chart_view = QtCharts.QChartView(chart)
+        # self.ui.chart_view.setRenderHint(QPainter.Antialiasing)
+        # self.ui.chart_view.chart().setTheme(QtCharts.QChart.ChartThemeDark)
         
-        # Add chart to container
-        self.ui.chart_view.setMinimumSize(QSize(0,300))
-        self.ui.top_word.addWidget(self.ui.chart_view)
+        # # Set chart size policy
+        # sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        # sizePolicy.setHorizontalStretch(0)
+        # sizePolicy.setVerticalStretch(0)
+        # sizePolicy.setHeightForWidth(self.ui.chart_view.sizePolicy().hasHeightForWidth())
+        # self.ui.chart_view.setSizePolicy(sizePolicy)
+        
+        # # Add chart to container
+        # self.ui.chart_view.setMinimumSize(QSize(0,300))
+        # self.ui.top_word.addWidget(self.ui.chart_view)
+        
         
     
 if __name__ == "__main__":
